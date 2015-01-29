71eeaae4cecf16635f25ff442f5a2bc0156f3aaf
Michael Day <mday299@yahoo.com>
2015-01-29 14:16:08 -0800

    GCS_Mavlink: Log receipt of MAV_CMD_OVERRIDE_GOTO in dataflash.

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -1163,10 +1163,9 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
 
             //don't let the payload do anything during landing stages
             } else if (flight_stage == AP_SpdHgtControl::FLIGHT_LAND_APPROACH ||
-                    flight_stage == AP_SpdHgtControl::FLIGHT_LAND_FINAL 
-                    //TODO: Consider GO_AROUND here when new landing code is finished
-                    /* ||
-                    flight_stage == AP_SpdHgtControl::FLIGHT_LAND_GO_AROUND */) { 
+                    flight_stage == AP_SpdHgtControl::FLIGHT_LAND_FINAL /* ||
+                    flight_stage == AP_SpdHgtControl::FLIGHT_LAND_GO_AROUND
+                    TODO: Consdier GO_AROUND when it's merged with dev. */) { 
                
                 result = MAV_RESULT_FAILED;    
             } else {
@@ -1184,7 +1183,12 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
                 //TODO: terrain following altitude
 
                 result = MAV_RESULT_ACCEPTED;
-            }
+
+                //log the MAV_CMD_OVERRIDE_GOTO receipt in data flash:
+                char mesg[64];
+                snprintf(mesg, 64, "GOTO T:%d Lat:%d Lon:%d Alt:%d", millis(), next_WP_loc.lat, next_WP_loc.lng, next_WP_loc.alt);
+                DataFlash.Log_Write_Message(mesg);
+            }                 
 
             break;
 
@@ -1460,7 +1464,7 @@ void GCS_MAVLINK::handleMessage(mavlink_message_t* msg)
                     break;
             }
             break;
-
+        
         case MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES: {
             if (is_equal(packet.param1,1.0f)) {
                 plane.gcs[chan-MAVLINK_COMM_0].send_autopilot_version(FIRMWARE_VERSION);
